03a314e894bffbe47a50bd0bf7dc685e8f4c4040
Kuroba <d1114181709@gm.lhu.edu.tw>
2025-01-07 12:24:29 +0800

    增加帳戶相關按鈕 尚未設置板面配置

--- resources/views/Statistics/index.blade.php
+++ resources/views/Statistics/index.blade.php
@@ -3,6 +3,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @guest
+        @if (Route::has('login'))
+            <li class="nav-item">
+            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+            </li>
+        @endif
+        
+        @if (Route::has('register'))
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+        </li>
+        @endif
+    @else
+        <ul class="butten">
+            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                {{ Auth::user()->name }}
+            </a>
+        </ul>
+
+        <ul class="butten" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="{{ route('logout') }}"
+               onclick="event.preventDefault();
+                         document.getElementById('logout-form').submit();">
+                {{ __('Logout') }}
+            </a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                @csrf
+            </form>
+        </ul>
+    @endguest
     <title>勞工生活與就業狀況調查</title>
     @include('Statistics.style')
 </head>
